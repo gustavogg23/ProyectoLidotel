0dfd2d9673e891879de372e6bbb89406e05dcd7f
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2023-12-02 14:16:59 -0400

    Agregando gitignore y Lidotel.pas

---
+++ Lidotel.pas
@@ -0,0 +1,22 @@
+program Lidotel; // Gustavo Gutiérrez y Juan García
+
+uses crt;
+
+type
+	datosHuesped = record
+		nombre: string[30];
+		cedula: string[11];
+		email: string[30];
+		telefono: string[12];
+		diasEstadia: integer;
+	end;
+	
+var 
+	huespedes: array of datosHuesped;
+	i, j, numPersonas: integer;
+
+BEGIN
+	
+	
+END.
+
